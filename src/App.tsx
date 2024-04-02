f96c1f669cb76638cf228fde75b47bea3ff52a9a
Rene Brandel <renbran@amazon.com>
2024-04-02 09:04:47 -0400

    reset to initial state

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,10 @@ function App() {
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map(todo => <li key={todo.id}>{todo.content}</li>)}
+        {todos.map(todo => <li
+          key={todo.id}>
+            {todo.content}
+          </li>)}
       </ul> 
       <div>
         🥳 App successfully hosted. Try creating a new todo.
